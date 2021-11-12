96d8e968cfca0c40276e1459b58a80030d9a3f67
SharkKnight4 <79238436+SharkKnight4@users.noreply.github.com>
2021-11-11 18:33:25 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ camera=document.getElementById("camera");
 Webcam.attach("camera");
 function takesnapshot(){
     Webcam.snap(function(data_uri){
-image
+        document.getElementById("result").innerHTML='<img id="selfie" src="'+data_uri+'"/>'
     });
 }
 console.log("ml5 version:",ml5.version);
-classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/9DdD11Lou/model.json',modelLoaded);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JvmdtE38E/model.json',modelLoaded);
 function modelLoaded(){
 console.log('model is loaded');
 }
@@ -26,7 +26,8 @@ console.error(error);
     }
     else{
         console.log(results);
-        document.getElementById("result_name").innerHTML="You are a " + results[0].label;
+        document.getElementById("result_name").innerHTML=results[0].label;
+        document.getElementById("result_accuracy").innerHTML=results[0].confidence.toFixed(3);
 
     }
 }
